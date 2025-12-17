488746d50342444cb2087a871a9c16104a944816
Z Xiong <zxiong4@student.cvtc.edu>
2025-12-16 18:36:47 -0600

    Build class and folder structures of skeleton

---
+++ UI/PokeQuest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokeQuest
+{
+    public partial class PokeQuest : Form
+    {
+        public PokeQuest()
+        {
+            InitializeComponent();
+        }
+    }
+}
