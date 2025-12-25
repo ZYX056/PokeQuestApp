034adbbac9568bfaff2b489bdfb98dc2dafab25f
Z Xiong <zxiong4@student.cvtc.edu>
2025-12-24 22:21:50 -0600

    Update UI elements

--- UI/PokeQuest.Designer.cs
+++ UI/PokeQuest.Designer.cs
@@ -376,7 +376,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(1465, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(408, 408);
+            this.dataGridView1.Size = new System.Drawing.Size(437, 408);
             this.dataGridView1.TabIndex = 24;
             // 
             // AddItemQtyTextBox
